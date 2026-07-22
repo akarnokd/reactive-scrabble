5434adf624dd02075173520b2b0fa112e9a61f76
akarnokd <akarnokd@gmail.com>
2026-07-22 18:35:28 +0200

    Add StreamEx and Vavr

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Reactive Scrabble Benchmarks
 
+:question: Why haven't I measured library X ???
+
+:left_arrow: I can't know or remember all data processing libraries of the Java ecosystem! PR welcome.
 
 ## Run a specific benchmark
 
@@ -9,7 +12,9 @@ gradlew jmh -PjmhIncludes=Helidon --rerun-tasks
 
 ## Results (alphabetic)
 
-i9 275HX (8P+16E, 2.7GHz+), 32GB LPDDR5 6400MT CL52, Windows 25H2, JDK 26.0.1
+**Specs:** i9 275HX (8P+16E, 2.7GHz+), 32GB LPDDR5 6400MT CL52, Windows 25H2, JDK 26.0.1
+
+**Timestamp:** 2026.07.22 16:00
 
 | Benchmark                              | Version |   Score |    Error | Units |
 |----------------------------------------|---------|--------:|---------:|-------|
@@ -40,6 +45,8 @@ i9 275HX (8P+16E, 2.7GHz+), 32GB LPDDR5 6400MT CL52, Windows 25H2, JDK 26.0.1
 | RxJava4Flowable                        | 4.0.0a21|  12,646 | ±  0,138 | ms/op |
 | RxJava4Observable                      | 4.0.0a21|  11,818 | ±  0,134 | ms/op |
 | RxJava4Streamable                      | 4.0.0a21|  19,190 | ±  0,187 | ms/op |
+| StreamExAmaembo                        | 0.9.0   |  12,444 | ±  0,192 | ms/op |
+| Vavr                                   | 1.0.1   |  48,369 | ±  0,631 | ms/op |
 
 ### by time, lower is better
 
@@ -58,6 +65,7 @@ i9 275HX (8P+16E, 2.7GHz+), 32GB LPDDR5 6400MT CL52, Windows 25H2, JDK 26.0.1
 | RxJava2Observable                      | 2.2.21  |  11,494 | ±  0,120 | ms/op |
 | RxJava3Observable                      | 3.1.12  |  11,707 | ±  0,121 | ms/op |
 | RxJava4Observable                      | 4.0.0a21|  11,818 | ±  0,134 | ms/op |
+| StreamExAmaembo                        | 0.9.0   |  12,444 | ±  0,192 | ms/op |
 | RxJava4Flowable                        | 4.0.0a21|  12,646 | ±  0,138 | ms/op |
 | RxJava2Flowable                        | 2.2.21  |  13,057 | ±  0,146 | ms/op |
 | RxJava3Flowable                        | 3.1.12  |  13,288 | ±  0,173 | ms/op |
@@ -69,6 +77,7 @@ i9 275HX (8P+16E, 2.7GHz+), 32GB LPDDR5 6400MT CL52, Windows 25H2, JDK 26.0.1
 | CyclopsReact                           | 2.1.1   |  21,071 | ±  0,455 | ms/op |
 | Mutiny                                 | 3.3.0   |  27,250 | ±  0,161 | ms/op |
 | JOOL                                   | 0.9.15  |  41,213 | ±  0,713 | ms/op |
+| Vavr                                   | 1.0.1   |  48,369 | ±  0,631 | ms/op |
 | RxJava1                                | 1.3.8   |  50,612 | ±  0,421 | ms/op |
 | Reactive4Java                          | 0.98.1  | 355,223 | ± 11,164 | ms/op |
 | AkkaStream                             | 2.13    | 268,331 | ±  6,197 | ms/op |
---
+++ src/jmh/java/hu/akarnokd/scrabble/benchmark/Vavr.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2019 José Paumard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package hu.akarnokd.scrabble.benchmark;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
+import org.openjdk.jmh.annotations.*;
+
+import hu.akarnokd.scrabble.support.ShakespearePlaysScrabble;
+import io.vavr.collection.Stream;
+
+/**
+ * Shakespeare plays Scrabble with Vavr.
+ * @author José
+ */
+public class Vavr extends ShakespearePlaysScrabble {
+
+    static Stream<Integer> chars(String word) {
+//      return Flowable.range(0, word.length()).map(i -> (int)word.charAt(i));
+      return Stream.range(0, word.length()).map(index -> (int)word.charAt(index));
+  }
+
+    @SuppressWarnings("unused")
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Warmup(
+        iterations = 5, time = 1
+    )
+    @Measurement(
+        iterations = 5, time = 1
+    )
+    @Fork(1)
+    public List<Entry<Integer, List<String>>> measureThroughput() {
+
+        // Function to compute the score of a given word
+        Function<Integer, Integer> scoreOfALetter = letter -> letterScores[letter - 'a'];
+
+        // score of the same letters in a word
+        Function<Entry<Integer, Long>, Integer> letterScore =
+                entry ->
+                    letterScores[entry.getKey() - 'a'] *
+                    Integer.min(
+                        entry.getValue().intValue(),
+                        scrabbleAvailableLetters[entry.getKey() - 'a']
+                    );
+
+
+        // Histogram of the letters in a given word
+        Function<String, Map<Integer, Long>> histoOfLetters =
+                word -> chars(word)
+                            .collect(
+                                Collectors.groupingBy(
+                                    Function.identity(),
+                                    Collectors.counting()
+                                )
+                            );
+
+        // number of blanks for a given letter
+        Function<Entry<Integer, Long>, Long> blank =
+                entry ->
+                    Long.max(
+                        0L,
+                        entry.getValue() -
+                        scrabbleAvailableLetters[entry.getKey() - 'a']
+                    );
+
+        // number of blanks for a given word
+        Function<String, Long> nBlanks =
+                word -> Stream.ofAll(histoOfLetters.apply(word)
+                            .entrySet())
+                            .map(blank)
+                            .sum().longValue();
+
+        // can a word be written with 2 blanks?
+        Predicate<String> checkBlanks = word -> nBlanks.apply(word) <= 2;
+
+        // score taking blanks into account
+        Function<String, Integer> score2 =
+                word -> Stream.ofAll(histoOfLetters.apply(word)
+                            .entrySet())
+                            .map(letterScore)
+                            .sum().intValue();
+
+        // Placing the word on the board
+        // Building the streams of first and last letters
+        Function<String, Stream<Integer>> first3 = word -> chars(word).take(3);
+        Function<String, Stream<Integer>> last3 = word -> chars(word).drop(Integer.max(0, word.length() - 4));
+
+        // Stream to be maxed
+        Function<String, Stream<Integer>> toBeMaxed =
+            word -> first3.apply(word).appendAll(last3.apply(word));
+
+        // Bonus for double letter
+        Function<String, Integer> bonusForDoubleLetter =
+            word -> toBeMaxed.apply(word)
+                        .map(scoreOfALetter)
+                        .max()
+                        .getOrElse(0);
+
+        // score of the word put on the board
+        Function<String, Integer> score3 =
+            word ->
+                 2 * (score2.apply(word) + bonusForDoubleLetter.apply(word))
+                 + (word.length() == 7 ? 50 : 0);
+
+        Function<Function<String, Integer>, Map<Integer, List<String>>> buildHistoOnScore =
+                score -> Stream.ofAll(shakespeareWords)
+                                .filter(scrabbleWords::contains)
+                                .filter(checkBlanks) // filter out the words that needs more than 2 blanks
+                                .collect(
+                                   Collectors.groupingBy(
+                                      score,
+                                      () -> new TreeMap<Integer, List<String>>(Comparator.reverseOrder()),
+                                      Collectors.toList()
+                                   )
+                                );
+
+
+        // best key / value pairs
+        List<Entry<Integer, List<String>>> finalList =
+                Stream.ofAll(buildHistoOnScore.apply(score3).entrySet())
+                    .take(3)
+                    .collect(Collectors.toList()) ;
+
+//        System.out.println(finalList) ;
+
+        return finalList ;
+    }
+    public static void main(String[] args) throws Exception {
+        Vavr s = new Vavr();
+        s.init();
+        System.out.println(s.measureThroughput());
+    }
+}
